b46c5ffdc0878918a4bf26368fbb3482d7500f86
Shashank <shashanknx@gmail.com>
2019-10-03 14:39:03 -0400

    messed around lol

--- food-finder/screens/FeedScreen.js
+++ food-finder/screens/FeedScreen.js
@@ -5,46 +5,96 @@ import Touchable from 'react-native-platform-touchable';
 import { Ionicons } from '@expo/vector-icons';
 import { ExpoLinksView } from '@expo/samples';
 
+// class FeedLinksView extends React.Component {
+//   render() {
+//     return ();
+//   }
+// }
+
 
 export default function FeedScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <FeedLinksView /> 
-
+      {}
       <ExpoLinksView />
     </ScrollView>
   );
 }
 
-export class FeedLinksView extends React.Component {
-  render() {
-    return (
-      <View>
-      <Text style={styles.feedText}>{post1}</Text>
-      </View>
-    );
-  }
-}
-
-
 FeedScreen.navigationOptions = {
   title: 'Feed',
 };
 
-const post1 = "Post 1";
-const post2 = "Post 2";
+
+      // <View>
+      //   <Text style={styles.optionsTitleText}>Resources</Text>
+      //
+      //   <Touchable
+      //     style={styles.option}
+      //     background={Touchable.Ripple('#ccc', false)}
+      //     onPress={this._handlePressDocs}>
+      //     <View style={{ flexDirection: 'row' }}>
+      //       <View style={styles.optionIconContainer}>
+      //         <Image
+      //           source={require('./assets/images/expo-icon.png')}
+      //           resizeMode="contain"
+      //           fadeDuration={0}
+      //           style={{ width: 20, height: 20, marginTop: 1 }}
+      //         />
+      //       </View>
+      //       <View style={styles.optionTextContainer}>
+      //         <Text style={styles.optionText}>Read the Expo documentation</Text>
+      //       </View>
+      //     </View>
+      //   </Touchable>
+      //
+      //   <Touchable
+      //     style={styles.option}
+      //     background={Touchable.Ripple('#ccc', false)}
+      //     onPress={this._handlePressForums}>
+      //     <View style={{ flexDirection: 'row' }}>
+      //       <View style={styles.optionIconContainer}>
+      //         <Ionicons name="ios-chatboxes" size={22} color="#ccc" />
+      //       </View>
+      //       <View style={styles.optionTextContainer}>
+      //         <Text style={styles.optionText}>Ask a question on the Expo forums</Text>
+      //       </View>
+      //     </View>
+      //   </Touchable>
+      // </View>
+
+  // _handlePressDocs = () => {
+  //   WebBrowser.openBrowserAsync('http://docs.expo.io');
+  // };
+  //
+  // _handlePressForums = () => {
+  //   WebBrowser.openBrowserAsync('http://forums.expo.io');
+  // };
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
   },
-  feedText: {
+  optionsTitleText: {
+    fontSize: 16,
+    marginLeft: 15,
+    marginTop: 9,
+    marginBottom: 12,
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionText: {
     fontSize: 15,
     marginTop: 1,
   },
--- food-finder/screens/RegisterScreen.js
+++ food-finder/screens/RegisterScreen.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { Input, Button, Icon } from 'react-native-elements';
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  Input,
+  Button,
+  Icon
+} from 'react-native-elements';
 
 export default class RegisterScreen extends React.Component {
   constructor(props) {
@@ -14,158 +24,331 @@ export default class RegisterScreen extends React.Component {
       showLoading: false,
     };
   }
-    validateEmail(email) {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  
-      return re.test(email);
-    }
+  validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    submitLoginCredentials() {
-      const { name, email, password } = this.state;
-      this.setState({
-        showLoading: true
-      });
-      //insert the register request here 
-    }
-  
+    return re.test(email);
+  }
+
+  submitLoginCredentials() {
+    const {
+      name,
+      email,
+      password
+    } = this.state;
+    this.setState({
+      showLoading: true
+    });
+    //insert the register request here
+  }
+
   render() {
-    const { name, email, password, email_valid, error_msg, showLoading } = this.state;
+    const {
+      name,
+      email,
+      password,
+      email_valid,
+      error_msg,
+      showLoading
+    } = this.state;
 
-  return (
-    <View style={styles.container}>
-        <View style={styles.loginView}>
-          <View style={styles.loginTitle}>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.travelText}>Food</Text>
-            </View>
-            <View style={{ marginTop: -10 }}>
-              <Text style={styles.travelText}>Finder</Text>
-            </View>
-            <View style={{marginTop: 10}}>
-              <Text style={styles.errorText}>{error_msg}</Text>
-            </View>
-          </View>
-          <View style={styles.loginInput}>
-            <Input
-              leftIcon={
-                <Icon
-                  name="user-o"
-                  type="font-awesome"
-                  color="rgba(171, 189, 219, 1)"
-                  size={25}
-                />
-              }
-              containerStyle={{ marginVertical: 10 }}
-              onChangeText={name => this.setState({ name })}
-              value={name}
-              inputStyle={{ marginLeft: 10, color: 'black' }}
-              keyboardAppearance="light"
-              placeholder="Name"
-              autoFocus={false}
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="default"
-              returnKeyType="next"
-              blurOnSubmit={false}
-              placeholderTextColor="black"
-            />
-            <Input
-              leftIcon={
-                <Icon
-                  name="user-o"
-                  type="font-awesome"
-                  color="rgba(171, 189, 219, 1)"
-                  size={25}
-                />
-              }
-              containerStyle={{ marginVertical: 10 }}
-              onChangeText={email => this.setState({ email })}
-              value={email}
-              inputStyle={{ marginLeft: 10, color: 'black' }}
-              keyboardAppearance="light"
-              placeholder="Email"
-              autoFocus={false}
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="email-address"
-              returnKeyType="next"
-              ref={input => (this.emailInput = input)}
-              onSubmitEditing={() => {
-                this.setState({ email_valid: this.validateEmail(email) });
-                this.passwordInput.focus();
-              }}
-              blurOnSubmit={false}
-              placeholderTextColor="black"
-              errorStyle={{ textAlign: 'center', fontSize: 12 }}
-              errorMessage={
-                email_valid ? null : 'Please enter a valid email address'
-              }
-            />
-            <Input
-              leftIcon={
-                <Icon
-                  name="lock"
-                  type="font-awesome"
-                  color="rgba(171, 189, 219, 1)"
-                  size={25}
-                />
-              }
-              containerStyle={{ marginVertical: 10 }}
-              onChangeText={password => this.setState({ password })}
-              value={password}
-              inputStyle={{ marginLeft: 10, color: 'black' }}
-              secureTextEntry={true}
-              keyboardAppearance="light"
-              placeholder="Password"
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="default"
-              returnKeyType="done"
-              ref={input => (this.passwordInput = input)}
-              blurOnSubmit={true}
-              placeholderTextColor="black"
-            />
-          </View>
-          <Button
-            title="REGISTER"
-            activeOpacity={1}
-            underlayColor="transparent"
-            onPress={this.submitLoginCredentials.bind(this)}
-            loading={showLoading}
-            loadingProps={{ size: 'small', color: 'black' }}
-            disabled={name.length == 0 || !email_valid || password.length < 8}
-            buttonStyle={{
-              height: 50,
-              width: 250,
-              backgroundColor: 'transparent',
-              borderWidth: 2,
-              borderColor: 'black',
-              borderRadius: 30,
-              marginLeft: 25,
-            }}
-            containerStyle={{ marginVertical: 10 }}
-            titleStyle={{ fontWeight: 'bold', color: 'black' }}
-          />
-          <View style={styles.footerView}>
-            <Text style={{ color: 'grey' }}>Already have an account?</Text>
-            <Button
-              title="Login"
-              type="clear"
-              activeOpacity={0.5}
-              titleStyle={{ color: 'black', fontSize: 15 }}
-              containerStyle={{ marginTop: -10 }}
-              onPress={() => this.props.navigation.navigate('Auth')}
-            />
-          </View>
-        </View>
-    </View>
+    return ( <
+      View style = {
+        styles.container
+      } >
+      <
+      View style = {
+        styles.loginView
+      } >
+      <
+      View style = {
+        styles.loginTitle
+      } >
+      <
+      View style = {
+        {
+          flexDirection: 'row'
+        }
+      } >
+      <
+      Text style = {
+        styles.travelText
+      } > Food < /Text> <
+      /View> <
+      View style = {
+        {
+          marginTop: -10
+        }
+      } >
+      <
+      Text style = {
+        styles.travelText
+      } > Finder < /Text> <
+      /View> <
+      View style = {
+        {
+          marginTop: 10
+        }
+      } >
+      <
+      Text style = {
+        styles.errorText
+      } > {
+        error_msg
+      } < /Text> <
+      /View> <
+      /View> <
+      View style = {
+        styles.loginInput
+      } >
+      <
+      Input leftIcon = {
+        <
+        Icon
+        name = "user-o"
+        type = "font-awesome"
+        color = "rgba(171, 189, 219, 1)"
+        size = {
+          25
+        }
+        />
+      }
+      containerStyle = {
+        {
+          marginVertical: 10
+        }
+      }
+      onChangeText = {
+        name => this.setState({
+          name
+        })
+      }
+      value = {
+        name
+      }
+      inputStyle = {
+        {
+          marginLeft: 10,
+          color: 'black'
+        }
+      }
+      keyboardAppearance = "light"
+      placeholder = "Name"
+      autoFocus = {
+        false
+      }
+      autoCapitalize = "none"
+      autoCorrect = {
+        false
+      }
+      keyboardType = "default"
+      returnKeyType = "next"
+      blurOnSubmit = {
+        false
+      }
+      placeholderTextColor = "black" /
+      >
+      <
+      Input leftIcon = {
+        <
+        Icon
+        name = "user-o"
+        type = "font-awesome"
+        color = "rgba(171, 189, 219, 1)"
+        size = {
+          25
+        }
+        />
+      }
+      containerStyle = {
+        {
+          marginVertical: 10
+        }
+      }
+      onChangeText = {
+        email => this.setState({
+          email
+        })
+      }
+      value = {
+        email
+      }
+      inputStyle = {
+        {
+          marginLeft: 10,
+          color: 'black'
+        }
+      }
+      keyboardAppearance = "light"
+      placeholder = "Email"
+      autoFocus = {
+        false
+      }
+      autoCapitalize = "none"
+      autoCorrect = {
+        false
+      }
+      keyboardType = "email-address"
+      returnKeyType = "next"
+      ref = {
+        input => (this.emailInput = input)
+      }
+      onSubmitEditing = {
+        () => {
+          this.setState({
+            email_valid: this.validateEmail(email)
+          });
+          this.passwordInput.focus();
+        }
+      }
+      blurOnSubmit = {
+        false
+      }
+      placeholderTextColor = "black"
+      errorStyle = {
+        {
+          textAlign: 'center',
+          fontSize: 12
+        }
+      }
+      errorMessage = {
+        email_valid ? null : 'Please enter a valid email address'
+      }
+      /> <
+      Input leftIcon = {
+        <
+        Icon
+        name = "lock"
+        type = "font-awesome"
+        color = "rgba(171, 189, 219, 1)"
+        size = {
+          25
+        }
+        />
+      }
+      containerStyle = {
+        {
+          marginVertical: 10
+        }
+      }
+      onChangeText = {
+        password => this.setState({
+          password
+        })
+      }
+      value = {
+        password
+      }
+      inputStyle = {
+        {
+          marginLeft: 10,
+          color: 'black'
+        }
+      }
+      secureTextEntry = {
+        true
+      }
+      keyboardAppearance = "light"
+      placeholder = "Password"
+      autoCapitalize = "none"
+      autoCorrect = {
+        false
+      }
+      keyboardType = "default"
+      returnKeyType = "done"
+      ref = {
+        input => (this.passwordInput = input)
+      }
+      blurOnSubmit = {
+        true
+      }
+      placeholderTextColor = "black" /
+      >
+      <
+      /View> <
+      Button title = "REGISTER"
+      activeOpacity = {
+        1
+      }
+      underlayColor = "transparent"
+      onPress = {
+        this.submitLoginCredentials.bind(this)
+      }
+      loading = {
+        showLoading
+      }
+      loadingProps = {
+        {
+          size: 'small',
+          color: 'black'
+        }
+      }
+      disabled = {
+        name.length == 0 || !email_valid || password.length < 8
+      }
+      buttonStyle = {
+        {
+          height: 50,
+          width: 250,
+          backgroundColor: 'transparent',
+          borderWidth: 2,
+          borderColor: 'black',
+          borderRadius: 30,
+          marginLeft: 25,
+        }
+      }
+      containerStyle = {
+        {
+          marginVertical: 10
+        }
+      }
+      titleStyle = {
+        {
+          fontWeight: 'bold',
+          color: 'black'
+        }
+      }
+      /> <
+      View style = {
+        styles.footerView
+      } >
+      <
+      Text style = {
+        {
+          color: 'grey'
+        }
+      } > Already have an account ? < /Text> <
+      Button title = "Login"
+      type = "clear"
+      activeOpacity = {
+        0.5
+      }
+      titleStyle = {
+        {
+          color: 'black',
+          fontSize: 15
+        }
+      }
+      containerStyle = {
+        {
+          marginTop: -10
+        }
+      }
+      onPress = {
+        () => this.props.navigation.navigate('Auth')
+      }
+      /> <
+      /View> <
+      /View> <
+      /View>
 
-  )}
-          }   
+    )
+  }
+}
 
-  RegisterScreen.navigationOptions = {
-    title: 'Register Page',
-  };
+RegisterScreen.navigationOptions = {
+  title: 'Register Page',
+};
 
 const styles = StyleSheet.create({
   container: {
