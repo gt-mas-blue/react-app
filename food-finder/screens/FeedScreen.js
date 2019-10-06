d507fb5d087b71eff74a8a53c84e793c18db5da7
Shashank <shashanknx@gmail.com>
2019-10-06 15:10:14 -0400

    Created DealInput, DealItem; Update FeedScreen
    I updated the FeedScreen for basic functionality. I also created files that can be used later to handle post creation

--- food-finder/screens/FeedScreen.js
+++ food-finder/screens/FeedScreen.js
@@ -1,23 +1,42 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import Touchable from 'react-native-platform-touchable';
-import { Ionicons } from '@expo/vector-icons';
-import { ExpoLinksView } from '@expo/samples';
-
-// class FeedLinksView extends React.Component {
-//   render() {
-//     return ();
-//   }
-// }
-
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, ScrollView, TextInput, Button, FlatList} from 'react-native';
 
 export default function FeedScreen() {
+  const [enteredDeal, setEnteredDeal] = useState('');
+  const [deals, setDeals] = useState([]);
+
+  const dealInputHandler = (enteredText) => {
+    setEnteredDeal(enteredText);
+  };
+
+  const addDealHandler = () => {
+    setDeals(currentDeals => [
+      ...currentDeals,
+      { key: Math.random().toString(), value: enteredDeal }
+    ]);
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      {}
-      <ExpoLinksView />
-    </ScrollView>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add Deal"
+          style={styles.input}
+          onChangeText = {dealInputHandler}
+          value={enteredDeal}
+          />
+        <Button title="ADD" onPress={addDealHandler} />
+      </View>
+      <FlatList
+      // keyExtractor function
+        data={deals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
@@ -25,77 +44,25 @@ FeedScreen.navigationOptions = {
   title: 'Feed',
 };
 
-
-      // <View>
-      //   <Text style={styles.optionsTitleText}>Resources</Text>
-      //
-      //   <Touchable
-      //     style={styles.option}
-      //     background={Touchable.Ripple('#ccc', false)}
-      //     onPress={this._handlePressDocs}>
-      //     <View style={{ flexDirection: 'row' }}>
-      //       <View style={styles.optionIconContainer}>
-      //         <Image
-      //           source={require('./assets/images/expo-icon.png')}
-      //           resizeMode="contain"
-      //           fadeDuration={0}
-      //           style={{ width: 20, height: 20, marginTop: 1 }}
-      //         />
-      //       </View>
-      //       <View style={styles.optionTextContainer}>
-      //         <Text style={styles.optionText}>Read the Expo documentation</Text>
-      //       </View>
-      //     </View>
-      //   </Touchable>
-      //
-      //   <Touchable
-      //     style={styles.option}
-      //     background={Touchable.Ripple('#ccc', false)}
-      //     onPress={this._handlePressForums}>
-      //     <View style={{ flexDirection: 'row' }}>
-      //       <View style={styles.optionIconContainer}>
-      //         <Ionicons name="ios-chatboxes" size={22} color="#ccc" />
-      //       </View>
-      //       <View style={styles.optionTextContainer}>
-      //         <Text style={styles.optionText}>Ask a question on the Expo forums</Text>
-      //       </View>
-      //     </View>
-      //   </Touchable>
-      // </View>
-
-  // _handlePressDocs = () => {
-  //   WebBrowser.openBrowserAsync('http://docs.expo.io');
-  // };
-  //
-  // _handlePressForums = () => {
-  //   WebBrowser.openBrowserAsync('http://forums.expo.io');
-  // };
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
+  screen: {
+    padding: 50
   },
-  optionsTitleText: {
-    fontSize: 16,
-    marginLeft: 15,
-    marginTop: 9,
-    marginBottom: 12,
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  optionIconContainer: {
-    marginRight: 9,
+  input: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 3,
+    padding: 10
   },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#EDEDED',
-  },
-  optionText: {
-    fontSize: 15,
-    marginTop: 1,
-  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    borderColor: "black",
+    borderWidth: 1,
+  }
 });
