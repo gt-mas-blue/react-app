1708b49e9336c54b3c2dc2b4386bdcb4d773b9d5
CKillens <cole.killen@gmail.com>
2019-11-18 15:21:34 -0500

    New Settings Page No Functionality

--- food-finder/screens/SettingsScreen.js
+++ food-finder/screens/SettingsScreen.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import SettingsList from 'react-native-setting-list';
+import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, Button} from 'react-native';
 import ReactNativeSettingsPage, { 
 	SectionRow, 
 	NavigateRow,
-	CheckRow
+  CheckRow
 } from 'react-native-settings-page';
 export default class SettingsScreen extends React.Component{
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
+  
+  constructor(){
+    super();
+    this.onValueChange = this.onValueChange.bind(this);
+    this.state = {switchValue: false};
+  }
+  
   render() {
-    return(
-      <ExpoConfigView></ExpoConfigView>
-    )
-  };
+    return (
+      <View style={{backgroundColor:'gray',flex:1}}>
+        <View style={{flex:1, marginTop:50}}>
+          <SettingsList>
+            <SettingsList.Header headerText='First Grouping' headerStyle={{color:'white'}}/>
+            <SettingsList.Item
+              hasNavArrow={false}
+              switchState={this.state.switchValue}
+              switchOnValueChange={this.onValueChange}
+              hasSwitch={true}
+              title='Switch Example'/>
+            <SettingsList.Header headerText='Login Information' headerStyle={{color:'white', marginTop:50}}/>
+            <SettingsList.Item titleInfo='Some Information' hasNavArrow={false} title='Username'/>
+            <SettingsList.Item titleInfo='Some Information' hasNavArrow={false} title='Password'/>
+            <SettingsList.Item titleInfo='Some Information' hasNavArrow={false} title='Email'/>
+          </SettingsList>
+        </View>
+      </View>
+    );
+  }
+  
+  onValueChange(value){
+    this.setState({switchValue: value});
+  }
 }
 
 SettingsScreen.navigationOptions = {
