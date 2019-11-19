167b2b32105c3e083185d4752b01f3e9fc19845e
Shashank <shashanknx@gmail.com>
2019-11-19 14:56:16 -0500

    Update DealInput.js

--- food-finder/components/DealInput.js
+++ food-finder/components/DealInput.js
@@ -55,40 +55,39 @@ const DealInput = props => {
       allowsEditing: true,
       base64: true,
       exif: true,
-
     })
-    //
-    // todo: Upload the image, passback the mongoID, store mongoID in field img:
+    if (!result.cancelled) {
+      Alert.alert("Success");
+    } else {
+      Alert.alert("Error!")
+    }
+
+    // Post Image
+    axios.post("https://foodfinderapi.herokuapp.com/Images/", {filepath: result.uri});
+
     var newDict = {
       title: enteredDeal.title,
       description: enteredDeal.description,
       author: enteredDeal.author,
-      img: result.base64,
-      obj_num: null
+      img: result.uri,
+      obj_num: null // set this to the image object number returned from Post Image
       };
-    ret = axios.post("https://foodfinderapi.herokuapp.com/Images/", {filepath: result.uri});
-
-    if (!result.cancelled) {
-      Alert.alert("Success");
-
-    } else {
-      Alert.alert("Error!")
-    }
 
     setEnteredDeal(newDict);
   }
 
   const addDealHandler = () => {
     props.onAddDeal(enteredDeal);
-    // this should be able to work
+
+    // Create Post
     axios.post("https://foodfinderapi.herokuapp.com/Posts/", {
-      username: AsyncStorage.getItem('username'),
-      postTitle: enteredDeal.title,
-      description: enteredDeal.description,
-      likes: 0,
-      imgPointer: enteredDeal.img
-    }).then(function (response) {console.log(response);});
-    console.log(enteredDeal);
+      "username": AsyncStorage.getItem('username'),
+      "postTitle": enteredDeal.title,
+      "description": enteredDeal.description,
+      "likes": 0,
+      "imgPointer": enteredDeal.obj_num
+    }).then(res => {console.log(res)});
+
     setEnteredDeal(dict);
   };
 
@@ -100,17 +99,21 @@ const DealInput = props => {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        {enteredDeal.img == "../assets/images/cardImage2.png" &&
         <Image
           style={{width: 50, height: 50}}
           source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
+          />
+        }
+        {enteredDeal.img != "../assets/images/cardImage2.png" &&
+        <Image
+          style={{width: 150, height: 150}}
+          source={{uri: enteredDeal.img}}
         />
+        }
         <Button
           title="Choose Photo"
           onPress={this.onChooseImagePress}
-        />
-        <Image
-          style={{width: 50, height: 50}}
-          source={{uri: enteredDeal.img}}
         />
         <TextInput
           placeholder="Title"
